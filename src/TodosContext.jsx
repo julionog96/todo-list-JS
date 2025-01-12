a22440933a69644be817ee11a3877d5db265b217
julio rodrigues <juliorn99@gmail.com>
2025-01-11 23:21:11 -0300

    cleaning the code

--- src/TodosContext.jsx
+++ src/TodosContext.jsx
@@ -1,5 +1,4 @@
-import { createContext, useReducer } from "react";
-import todosReducer from './TodosReducer.js';
+import { createContext, useReducer, useContext } from "react";
 export const TodosContext = createContext("");
 
 const initialTodos = [
@@ -22,4 +21,32 @@ export function TodosProvider({children}) {
       </main>
     </>
   )
+}
+
+
+export function useTodos() {
+  return useContext(TodosContext);
+}
+
+function todosReducer(todos,action) {
+
+  switch (action.type) {
+      case 'deleted': {
+          if (confirm('Are you sure you want to delete the to-do?')) {
+              return todos.filter(todo => todo.id !== action.id);
+            }
+      }
+
+      case 'toggledIsDone': {
+          return todos.map(todo => {
+              if (todo.id === action.id) {
+                todo.isDone = !todo.isDone;
+                return todo;
+              } else {
+                return todo;
+              }
+            });
+      }
+  }
+
 }
